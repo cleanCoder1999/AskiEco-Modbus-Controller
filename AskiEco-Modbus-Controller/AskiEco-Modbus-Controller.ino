c4c711a8a1aac8b0c69e46959e21166c19ce41c5
Lucas Müllner <lucasmuellner@gmx.at>
2019-09-13 09:06:12 +0200

    Add project files.

---
+++ AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AskiEco_Modbus_Controller.ino
+ Created:	9/13/2019 9:06:05 AM
+ Author:	Lucas
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
