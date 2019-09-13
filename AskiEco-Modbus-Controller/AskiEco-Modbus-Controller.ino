0de86e63e7f4186dbaca5dcf80fe99685198246f
Lucas Müllner <lucasmuellner@gmx.at>
2019-09-13 13:16:58 +0200

    commit first changes

--- AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
+++ AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
@@ -4,13 +4,26 @@
  Author:	Lucas
 */
 
+#include "modbus.h"
+
+
+modbus_t* masterDescriptor;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
+	
+	masterDescriptor = modbus_new_tcp("192.168.100.49", 50000);
 
+	if()
+
+	
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+
+	
+
+
 }
