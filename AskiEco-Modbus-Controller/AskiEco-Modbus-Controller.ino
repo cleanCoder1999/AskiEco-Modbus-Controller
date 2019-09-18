429235870b68330ebe3149063158d2d0f6293b4d
Lucas Müllner <lucasmuellner@gmx.at>
2019-09-18 17:08:44 +0200

    added some documentation files

--- AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
+++ AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
@@ -13,12 +13,17 @@
 EthernetClient client;
 ModbusTCPClient* arduino;
 
-const byte arduinoMacAddress[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x94, 0xB5 };
+// MODBUS
+const int SLAVE_ADDRESS = 1;
+long valueOfHoldingReg = -1;
+int requestedValues = -1;
 
+// NETWORK
+const byte arduinoMacAddress[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x94, 0xB5 };
 IPAddress arduinoIp;
 const IPAddress askiEcoIp(192, 168, 100, 49);
-
 const uint16_t port = 502;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -36,42 +41,54 @@ void setup() {
 	Serial.println(arduinoIp);
 
 	establishTcpConnection();
-	if (client.connected())
+	if (client.connected() == 1)
 		Serial.println("[INFO]: in setup: client did connect via TCP");
 	else
 		Serial.println("[ERROR]: in setup: client did NOT connect via TCP");
 
 	establishModbusConnection();
-	if (arduino->connected())
+	if (arduino->connected() == 1)
 		Serial.println("[INFO]: in setup: Modbus connection to AskiEco-Controller via TCP established");
 	else
 		Serial.println("[ERROR]: in setup: could not connect to AskiEco-Controller via TCP");
 
+
 	/*
-	ModbusTCPClient arduino(client);
-
-	// set up connection
-	arduino.begin(askiEcoIp, port);
-
-	if (arduino.connected())
-		Serial.print("[INFO]: in setup: connection to AskiEco-Controller established");
-	else
-		Serial.print("[ERROR]: in setup: couldn't connect to AskiEco-Controller");
-
+	int availableValues = arduino->available();
+	Serial.print("[INFO]: in setup: number of available values to read: ");
+	Serial.println(availableValues);
 	*/
+
+	/*int readableValues = arduino->requestFrom(SLAVE_ADDRESS, HOLDING_REGISTERS, 432, 8);
+	Serial.print("[INFO]: in setup: number of values to read: ");
+	Serial.println(readableValues);
+	*/
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
 
+	//valueOfHoldingReg = arduino->holdingRegisterRead(1, 432);
+	requestedValues = arduino->requestFrom(1, HOLDING_REGISTERS, 22, 1);
+
+	delay(1000ul);
+
+
+	Serial.print("[INFO]: in loop: requestedValues=");
+	//Serial.println(valueOfHoldingReg);
+	Serial.println(requestedValues);
+
+
 
 	/*
 	* to be continued...
 	* */
+	/*
 	long valueOfHoldingReg = arduino->holdingRegisterRead(400);
 	Serial.print("[INFO]: in loop: valueOfHoldingReg=");
 	Serial.println(valueOfHoldingReg);
-
+	*/
 }
 
 
