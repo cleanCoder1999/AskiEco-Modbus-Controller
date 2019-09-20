c6433d0d357acd572ada3963752b233c03be7034
Lucas Müllner <lucasmuellner@gmx.at>
2019-09-20 11:11:23 +0200

    altered setup function in order to consume less memory

--- AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
+++ AskiEco-Modbus-Controller/AskiEco-Modbus-Controller.ino
@@ -4,9 +4,7 @@
  Author:	Lucas
 */
 
-#include <ArduinoRS485.h>
 #include <ArduinoModbus.h>
-
 #include <Ethernet.h>
 
 /*
@@ -16,13 +14,16 @@ EthernetClient client;
 //ModbusTCPClient* arduino;
 ModbusTCPClient arduino(client);
 
+// INITIALISING
+bool initReturnValues[4]{false, false, false, false };
+
 // MODBUS
-const int SLAVE_ADDRESS = 1;
-long valueOfHoldingReg = -1;
-int requestedValues = -1;
+const int SLAVE_ADDRESS{ 1 };
+long valueOfHoldingReg{ -1 };
+int requestedValues{ -1 };
 
 // NETWORK
-const byte arduinoMacAddress[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x94, 0xB5 };
+byte arduinoMacAddress[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x94, 0xB5 };
 IPAddress arduinoIp(172, 16, 17, 40);
 IPAddress subnet(255, 255, 0, 0);
 IPAddress gateWay(172, 16, 17, 1);
@@ -30,89 +31,107 @@ IPAddress dnsServer(192, 168, 100, 1);
 
 IPAddress askiEcoIp(192, 168, 100, 49);
 
-uint16_t port = 502;
+uint16_t port{ 502 };
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	
+
+	/*
+	 * INIT SERIAL INTERFACE (MONITOR)
+	 * */
 	initialiseSerialMonitor();
-	Serial.println("\n[INFO]: in setup: initialised serial monitor");
+	if(Serial)
+		initReturnValues[0] = true;
 
-	initialiseNetwork();
-	Serial.println("[INFO]: in setup: initialised network lib");
+	//Serial.println("[INFO]: in setup: initalising...");
 
-	arduinoIp = getArduinoIp();
-	
+	/*
+	 * INIT ETHERNET SHIELD
+	 * */
+	initReturnValues[1] = (bool) initialiseNetworkDHCP();
+	delay(2000);
+	if (!initReturnValues[1])
+	{
+		initialiseNetworkStatic();
+		delay(2000);
+
+		arduinoIp = {0, 0, 0, 0};
+		arduinoIp = getArduinoIp();
+		initReturnValues[1] = ipObtained();
+	}
+
 	// print configured IP
 	Serial.print("\nlocal IP: ");
 	Serial.println(arduinoIp);
 
 	/*
+	 * CONNECT TO ETHERNET SERVER
+	 * */
 	establishTcpConnection();
+	delay(2000);
 	if (client.connected() == 1)
-		Serial.println("[INFO]: in setup: client did connect via TCP");
+		initReturnValues[2] = true;
 	else
-		Serial.println("[ERROR]: in setup: client did NOT connect via TCP");
-	*/
-
-	//establishModbusConnection();
-	arduino.begin(askiEcoIp, port);
-	if (arduino.connected() == 1)//arduino->connected() == 1)
-		Serial.println("[INFO]: in setup: Modbus connection to AskiEco-Controller via TCP established");
+		initReturnValues[2] = false;
+	
+	/*
+	 * BEGIN CONNECTION TO MODBUS SLAVE
+	 * */
+	establishModbusConnection();
+	delay(2000);
+	if(client.connected())
+		initReturnValues[3] = true;
 	else
-		Serial.println("[ERROR]: in setup: could not connect to AskiEco-Controller via TCP");
-
+		initReturnValues[3] = false;
 
 	/*
-	int availableValues = arduino->available();
-	Serial.print("[INFO]: in setup: number of available values to read: ");
-	Serial.println(availableValues);
-	*/
+	 * CHECK IF INIT SUCCEEDED
+	 * */
+	bool everythingWorked{ true };
+	for (unsigned int i = 0; i < sizeof(initReturnValues) / sizeof(bool); ++i)
+	{
+		if (!initReturnValues[i])
+			everythingWorked = false;
+	}
 
-	/*int readableValues = arduino->requestFrom(SLAVE_ADDRESS, HOLDING_REGISTERS, 432, 8);
-	Serial.print("[INFO]: in setup: number of values to read: ");
-	Serial.println(readableValues);
-	*/
-
+	if (everythingWorked)
+		Serial.println("[INFO]: init succeeded");
+	else
+		Serial.println("[INFO]: init did NOT succeed");
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
 
+	/*
+	 * to be continued...
+	 * */
+
 	//valueOfHoldingReg = arduino->holdingRegisterRead(1, 432);
 	//requestedValues = arduino->requestFrom(1, HOLDING_REGISTERS, 22, 2);
-	requestedValues = arduino.requestFrom(1, HOLDING_REGISTERS, 22, 2);
+	//requestedValues = arduino.requestFrom(1, HOLDING_REGISTERS, 22, 2);
 	
+	//delay(1000ul);
 
-	delay(1000ul);
-
-
-	Serial.print("[INFO]: in loop: requestedValues=");
+	//Serial.print("[INFO]: in loop: requestedValues=");
 	//Serial.println(valueOfHoldingReg);
-	Serial.println(requestedValues);
-
-
-
-	/*
-	* to be continued...
-	* */
-	/*
-	long valueOfHoldingReg = arduino->holdingRegisterRead(400);
-	Serial.print("[INFO]: in loop: valueOfHoldingReg=");
-	Serial.println(valueOfHoldingReg);
-	*/
+	//Serial.println(requestedValues);
 }
-
 
 void initialiseSerialMonitor()
 {
 	Serial.begin(9600);
 	delay(1000);
 }
-void initialiseNetwork()
+
+bool initialiseNetworkDHCP()
 {
-	//Ethernet.begin(arduinoMacAddress);// , arduinoIp);
+	return (bool) Ethernet.begin(arduinoMacAddress);
+}
+
+void initialiseNetworkStatic()
+{
 	Ethernet.begin(arduinoMacAddress, arduinoIp, dnsServer, gateWay, subnet);
 }
 
@@ -121,21 +140,27 @@ IPAddress getArduinoIp()
 	return Ethernet.localIP();
 }
 
+bool ipObtained()
+{
+	IPAddress compareIp(0,0,0,0);
+	return arduinoIp != compareIp;
+}
+
 void establishTcpConnection()
 {
 	// connect client to server
 	client.connect(askiEcoIp, port);
 	//client.connect({172,16,17,39}, port);
 }
+
 void establishModbusConnection()
 {
 	// allocate dynamic object
 	//arduino = new ModbusTCPClient(client);
 	
 	// set up Modbus connection
-	//arduino->begin(askiEcoIp, port);
+	arduino.begin(askiEcoIp, port);
 }
-
 
 
 void closeConnectionToAskiEco()
@@ -145,17 +170,12 @@ void closeConnectionToAskiEco()
 }
 void closeModbusConnection()
 {
-	//if(arduino->connected())
-		//arduino->end();
+	if(arduino.connected())
+		arduino.end();
 }
 void closeTcpConnection()
 {
 	if(client.connected())
 		client.stop();
 }
-/*
-void printInfo(const char* info, unsigned int lengthOfInfo)
-{
-	Serial.println("[INFO]: " + *info);
-}*/
 
